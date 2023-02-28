780a21375deaa9b2444416f22c16ee90155b114c
Hassan Ndam <ndamfiverr@gmail.com>
2023-02-28 16:19:32 +0100

    add favicon

--- wp-content/themes/Sailing Loc/header.php
+++ wp-content/themes/Sailing Loc/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SailingLoc - Le meilleur de la location de bateaux </title>
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/icone/icone.png " type="image/x-icon">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/icone/icone.svg" type="image/x-icon">
 
     <?php wp_head();?>
 </head>
