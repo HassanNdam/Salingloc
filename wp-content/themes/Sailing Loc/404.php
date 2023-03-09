aef2a501ca96207cea2a4c7f09d22ad6fbbda045
Hassan Ndam <ndamfiverr@gmail.com>
2023-03-09 11:23:49 +0100

    404 ok

---
+++ wp-content/themes/Sailing Loc/404.php
@@ -0,0 +1,15 @@
+<?php get_header();?>
+
+    <div class="container mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center">
+                <h1 class="display-5 bleu3">Page introuvable</h1>
+                <h1 class="display-1">404</h1>
+                <p class="lead mb-4">Oops! Il semble que vous ayez pris un mauvais chemin.</p>
+                <a href="<?php echo get_site_url(); ?>" class="btn btn-warning" title="Revenir à la page d'accueil">Revenir à la page d'accueil</a>
+            </div>
+        </div>
+    </div>
+
+
+<?php get_footer();?>
