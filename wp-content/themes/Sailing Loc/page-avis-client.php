ca0c480b98ac57a1d6f2ce86891c0d723c2a422a
Hassan Ndam <ndamfiverr@gmail.com>
2023-03-09 11:19:14 +0100

    Add page avis & Blocks

---
+++ wp-content/themes/Sailing Loc/page-avis-client.php
@@ -0,0 +1,105 @@
+<?php get_header();  ?>
+
+
+<div class="container mb-5 text-center p-5">
+    <h3 class="mb-5 text-center fs-1">Ils <span class="orange">adorent</span> SailingLoc !</h3>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card mb-3 bg-white shadow-sm border-0">
+                <div class="card-header">
+                    <h4 class="card-title fs-3">Un service client au top</h4>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Le service client a été excellent du début à la fin. J'ai eu un problème avec
+                        ma commande et il a été résolu rapidement et efficacement. Je recommande vivement ce site pour
+                        son excellent service client..</p>
+                    <div class="rating orange">
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                    </div>
+
+                    <p class="card-text"><small class="text-muted">Posté par Jean le 1er janvier 2022</small></p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/avis-1.jpg"
+                        class="rounded-circle mr-3" alt="Avatar de l'utilisateur">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-3 bg-white shadow-sm border-0">
+                <div class="card-header">
+                    <h4 class="card-title fs-3">Excellent service</h4>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">J'ai été impressionné par la qualité de l'accueil et la rapidité de la
+                        de la prise en charge pour la location d'un bateau. Je recommande ce site à tous mes amis !</p>
+                    <div class="rating orange">
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                    </div>
+                    <p class="card-text"><small class="text-muted">Posté par Hassan le 15 février 2023</small></p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/avis-2.jpg"
+                        class="rounded-circle mr-3" alt="Avatar de l'utilisateur">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="card mb-3 bg-white shadow-sm border-0">
+                <div class="card-header">
+                    <h4 class="card-title fs-3">Très bon produit</h4>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Je suis très satisfait de mon achat. La livraison a été rapide et le produit
+                        est conforme à mes attentes. Je recommande ce site sans hésitation. Foncez! Vous allez kiffer !
+                    </p>
+                    <div class="rating orange">
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                    </div>
+                    <p class="card-text"><small class="text-muted">Posté par Mohamed le 1er janvier 2023</small></p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/avis-3.jpg"
+                        class="rounded-circle mr-3" alt="Avatar de l'utilisateur">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-3 bg-white shadow-sm border-0">
+                <div class="card-header">
+                    <h4 class="card-title fs-3">Excellent service client</h4>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">J'ai offert un weekend à Mon Fils et il l'adore ! Les capitains sont vraiment
+                        sympa. Nous reviendrons. Je recommande vivement ce produit !.</p>
+                    <div class="rating orange">
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9734;</span>
+                    </div>
+                    <p class="card-text"><small class="text-muted">Posté par Patrick le 15 février 2022</small></p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/avis-1.jpg"
+                        class="rounded-circle mr-5" alt="Avatar de l'utilisateur">
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="#" class="btn btn-warning text-dark btn-lg mt-5" title="Consulter FAQ - Je suis locataire de bateau">J'en
+        profite aussi</a>
+</div>
+
+
+
+
+
+<?php get_footer(); ?>
