086723bad5f5b75be1256dc5ae7f5606307444fb
Hassan Ndam <ndamfiverr@gmail.com>
2023-03-16 18:23:05 +0100

    Add pages

--- wp-content/themes/Sailing Loc/front-page.php
+++ wp-content/themes/Sailing Loc/front-page.php
@@ -1,20 +1,13 @@
 <?php get_header();?>
 
 
-<!-- Affichage - Nos bateaux Sailing Loc -->
-<div class="container">
-    <h3 class="mb-5 text-center fs-1">Nos <span class="bleu2">meilleurs</span> bateaux !</h3>
-</div>
-
-<!-- Affichage - Nos destinations Sailing Loc -->
-<div class="container">
-    <h3 class="mb-5 text-center fs-1">Nos  <span class="bleu2">destinations </span> de charme !</h3>
-
-</div>
-
-<!-- Appelle des différentes pages -->
+<!-- Appelle des différentes pages & blocs-->
+<?php get_template_part('template-parts/content-liste-bateaux'); ?>
+<?php get_template_part('template-parts/content-nos-destinations'); ?>
 <?php get_template_part('template-parts/content-a-propos'); ?>
 <?php get_template_part('template-parts/content-blog'); ?>
+<?php get_template_part('template-parts/content-activity'); ?>
 <?php include 'page-avis-client.php';?>
+
 
 <?php get_footer();?>
